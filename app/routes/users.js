04f1518621b438d0901a2dda99f928b0a81e7bd0
Aimee Knight <aimeeknight83@gmail.com>
2014-03-08 12:49:19 -0600

    implement user registration email

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,6 +3,7 @@
 var User = require('../models/user');
 var Item = require('../models/item');
 var gravatar = require('gravatar');
+var request = require('request');
 
 exports.auth = function(req, res){
   res.render('users/auth', {title: 'User Authentication'});
@@ -15,7 +16,16 @@ exports.register = function(req, res){
     user.insert(function(){
       console.log('>>>>>>>>', user);
       if(user._id){
-        res.redirect('/');
+        var key = process.env.MAILGUN;
+        var url = 'https://api:'+key+'@api.mailgun.net/v2/sandbox36742.mailgun.org/messages';
+        var post = request.post(url, function(err, response, body){
+          res.redirect('/');
+        });
+        var form = post.form();
+        form.append('from', 'aimeemarieknight@gmail.com');
+        form.append('to', user.email);
+        form.append('subject', 'Thank you for registering with NashBA!');
+        form.append('text', 'Thank you for registering with the Nashville Barter Association. Happy Bartering.');
       }else{
         res.render('users/auth', {title: 'User Authentication'});
       }
@@ -50,7 +60,6 @@ exports.show = function(req, res){
   User.findById(req.session.userId, function(user){
     var url = gravatar.url(user.email, {s: '200', r: 'pg', d: '404'});
     Item.findByUserId(req.session.userId, function(items){
-      console.log('ITEMS!!!!!!',items);
       res.render('users/show', {user:user, items:items, gravatar: url});
     });
   });
